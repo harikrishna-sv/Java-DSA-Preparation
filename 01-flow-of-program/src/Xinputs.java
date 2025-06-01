f1ab2c89a60bd1dbfe48aeede5e432c7c3ba7176
Hari Krishna G <148740247+harikrishna-sv@users.noreply.github.com>
2025-06-01 17:11:24 +0530

    Update Xinputs.java

--- 01-flow-of-program/src/Xinputs.java
+++ 01-flow-of-program/src/Xinputs.java
@@ -26,3 +26,36 @@ public class Xinputs {
 
     }
 }
+
+//one more logic
+//import java.util.Scanner;
+//
+//public class Xinputs {
+//    public static void main(String[] args) {
+//        Scanner obj = new Scanner(System.in);
+//        int sum = 0;
+//
+//        while (true) {
+//            System.out.println("Enter a number (or 'x' to stop):");
+//            String input = obj.next(); // Take input as a String
+//
+//            // Check if the input is 'x'
+//            if (input.equalsIgnoreCase("x")) {
+//                break; // Exit the loop if 'x' is entered
+//            }
+//
+//            try {
+//                // Convert the input to an integer and add to sum
+//                int num = Integer.parseInt(input);
+//                sum += num;
+//            } catch (NumberFormatException e) {
+//                // If input is not a number, print error message
+//                System.out.println("Invalid input. Please enter a valid number.");
+//            }
+//        }
+//
+//        System.out.println("The sum of all numbers is: " + sum); // Print the sum
+//
+//        obj.close(); // Close the scanner
+//    }
+//}
